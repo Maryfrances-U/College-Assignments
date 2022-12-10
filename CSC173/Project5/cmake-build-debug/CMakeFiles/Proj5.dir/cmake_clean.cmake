1718aaf2efccec19002276f557c568330b76aabf
Maryfrances Umeora <mumeora@u.rochester.edu>
2022-12-09 18:09:05 -0600

    cs173

---
+++ CSC173/Project5/cmake-build-debug/CMakeFiles/Proj5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proj5.dir/Boolean.c.o"
+  "CMakeFiles/Proj5.dir/Circuit.c.o"
+  "CMakeFiles/Proj5.dir/Gates.c.o"
+  "CMakeFiles/Proj5.dir/main.c.o"
+  "Proj5"
+  "Proj5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Proj5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
